955faa4569659b071fc59db433ccdc4437885177
Youssef <youssef@example.com>
2026-02-14 14:34:35 +0100

    Initial commit

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC0QuEDhruGrmf14nb__tE2L75BDrMRwPQ",
+    authDomain: "zina-coffee.firebaseapp.com",
+    projectId: "zina-coffee",
+    storageBucket: "zina-coffee.firebasestorage.app",
+    messagingSenderId: "831057835426",
+    appId: "1:831057835426:web:a1da12b127a9ddb2d5aefb",
+    measurementId: "G-PJ5MEV8N1Q"
+};
